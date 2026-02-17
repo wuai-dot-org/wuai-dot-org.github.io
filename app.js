f5b63fd4e4e915202f17f4f2f1a04165f579350e
KylezXing <110474093+KylezXing@users.noreply.github.com>
2026-02-17 16:18:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// 【新增 1】背景元素缓慢旋转动画，增加呼吸感
-gsap.to('.shape-1', {
-    rotation: 360,
-    duration: 120, // 非常慢的旋转，120秒一圈
-    repeat: -1,    // 无限循环
-    ease: "none"   // 线性匀速
-});
-
-gsap.to('.shape-2', {
-    rotation: -360, // 反向旋转
-    duration: 150,
-    repeat: -1,
-    ease: "none"
-});
 
 
 // 首屏入场动画
